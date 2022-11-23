f74c41495f56db1df847242a9574437285beede6
s-grundner <simon.grundner04@htl-salzburg.ac.at>
2022-11-24 00:20:29 +0100

    Update git_stash.sh

--- git_stash.sh
+++ git_stash.sh
@@ -1,6 +1,10 @@
-read -p "Are you sure? [y/n]" -n 1 -r
+read -p "Alle Änderungen werden verworfen. Weiter? [y/n]" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	git stash
+	echo "Änderungen wurden gestasht."
+else
+	echo "Änderungen wurden beibehalten."
 fi
+read -n 1 -s -r -p "Beliebige taste Drücken ..."
