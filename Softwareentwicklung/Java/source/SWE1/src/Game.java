bbce413ace5ec07397ee41d88e403634fea424b3
Smino <simon.grundner@gmail.com>
2023-10-16 16:10:52 +0200

    vault backup: 2023-10-16 16:10:51

--- Softwareentwicklung/Java/source/SWE1/src/Game.java
+++ Softwareentwicklung/Java/source/SWE1/src/Game.java
@@ -3,7 +3,13 @@ public class Game {
 		Out.print("Please enter your name: ");
 		String heroName = In.readString();
 
-		Out.print("How old is your hero");
-		In.readInt();
+		Out.print("How old is your hero?");
+		int age = In.readInt();
+
+		Out.print("How much can he/she hero? ");
+		double weight = In.readDouble();
+
+		String template = "Your hero is called %s, is %d years old and can carry %lf kg";
+		String.format
 	}
 }
