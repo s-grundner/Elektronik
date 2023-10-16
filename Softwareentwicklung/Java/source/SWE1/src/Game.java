4fec29381086c138c27cbb9c0939eab43f785c15
Smino <simon.grundner@gmail.com>
2023-10-16 16:00:49 +0200

    vault backup: 2023-10-16 16:00:49

---
+++ Softwareentwicklung/Java/source/SWE1/src/Game.java
@@ -0,0 +1,9 @@
+public class Game {
+	public static void main(String[] args) {
+		Out.print("Please enter your name: ");
+		String heroName = In.readString();
+
+		Out.print("How old is your hero");
+		In.readInt();
+	}
+}
