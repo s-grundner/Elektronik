af1cbbdb1bbec00d4826f286bb702e5817917428
Smino <simon.grundner@gmail.com>
2024-04-25 14:33:23 +0200

    vault backup: 2024-04-25 14:33:23

--- 00_templates/Scripts/get_ue.js
+++ 00_templates/Scripts/get_ue.js
@@ -1,25 +1,34 @@
 async function ws23(tp) {
 	const lva = ["Mathematik 1", "Einf. ET", "Softwareentwicklung 1", "Physik für TechnikerInnen"];
 	let paste_lva = await tp.system.suggester(lva, lva, true, "LVA Name");
-	switch (paste_lva) {
-		case value:
-			
-			break;
-	
-		default:
-			break;
-	}
 	return paste_lva;
 }
 
 async function ss24(tp) {
 	const lva = ["Mathematik 2", "Elektrotechnik", "Softwareentwicklung 2", "Algorithmen und Datenstrukturen"];
 	let paste_lva = await tp.system.suggester(lva, lva, true, "LVA Name");
+	switch (paste_lva) {
+		case lva[0]:
+
+
+			break;
+
+		default:
+			break;
+	}
 	return paste_lva;
 }
 
-function get_ue(tp) {
-	return { ws23, ss24 }
+function get_ue(tp, yyyy) {
+	let ret = "ASDHJKSHFSDHFSDHJF";
+	switch (yyyy) {
+		case 2023:
+			return ws23(tp)
+		case 2024:
+			return ss24(tp);
+		default:
+			break;
+	}
 }
 
-module_exports = get_ue
+module.exports = get_ue
---
+++ Untitled.md
@@ -0,0 +1,13 @@
+# UE1
+
+⬅️ <a href="UE0.md" class="internal-link">letzte Übung</a> | <a href="UE2.md" class="internal-link">nächste Übung</a> ➡️ 
+
+---
+
+🔎 [Aufgaben Sammlung](Aufgabensammlung_6.0.pdf)
+📝 [Angabe](./Angaben/Uebungsblatt1.pdf)
+✅ [Lösung](./Loesungen/UE1-k12136610.pdf)
+
+---
+
+
