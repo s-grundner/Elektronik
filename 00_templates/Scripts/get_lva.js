e7b68bfd9ddb4ca44439fb1fec52859db0e38ae8
s-grundner <simon.grundner@gmail.com>
2024-08-14 19:56:27 +0200

    vault backup: 2024-08-14 19:56:27 from PC-Simon

--- 00_templates/Scripts/get_lva.js
+++ 00_templates/Scripts/get_lva.js
@@ -16,6 +16,12 @@ async function ss24(tp) {
 	return paste_lva;
 }
 
+async function ws25(tp) {
+	const lva = ["Mathematik 3", "Einführung Elektronik", "EMV und SI", "Signale und Systeme", "Hardwaredesign", "Systems Programming", "HF-Systemtechnik 1"];
+	let paste_lva = await tp.system.suggester(lva, lva, true, "LVA Name");
+	return paste_lva;
+}
+
 function get_lva() {
 	return { type, ws23, ss24, ws25 }
 }
